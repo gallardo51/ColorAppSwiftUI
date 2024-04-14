ee53e98b7d85f0ea888515c0cd86de7c7640d4b1
Alex Sobolev <31108@mail.ru>
2024-04-14 18:04:12 +0300

    Initial Commit

---
+++ ColorAppSwiftUI/ColorAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppSwiftUIApp.swift
+//  ColorAppSwiftUI
+//
+//  Created by Александр Соболев on 14.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
